ebe5910a1390eacf89c983d74d079d4452eb633b
David Lundgren <dlundgren@syberisle.net>
2018-09-18 03:27:05 -0500

    Added MutableContext

---
+++ src/Output/Render/MutableContext.php
@@ -0,0 +1,46 @@
+<?php
+namespace Magnum\Output\Render;
+
+use Interop\Output\Context;
+
+class MutableContext
+	implements Context
+{
+	/**
+	 * List of data for this context
+	 *
+	 * @var array
+	 */
+	private $data;
+
+	public function set($name, $value)
+	{
+		$this->data[$name] = $value;
+	}
+
+	/**
+	 * @param array $data
+	 */
+	public function __construct(array $data = [])
+	{
+		$this->data = $data;
+	}
+
+	/**
+	 * Always returns true
+	 *
+	 * {@inheritdoc}
+	 */
+	public function accepts($template)
+	{
+		return true;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function provide($template)
+	{
+		return $this->data;
+	}
+}
