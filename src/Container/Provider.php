caaf6c2c354b74c85c2a7553789cd2f072fc8998
David Lundgren <dlundgren@syberisle.net>
2018-11-12 20:35:03 -0600

    Override the hints so Factories can be defined

--- src/Container/Provider.php
+++ src/Container/Provider.php
@@ -2,7 +2,11 @@
 
 namespace Magnum\Container;
 
+use Magnum\Container\Config\FactoryHint;
 use WoohooLabs\Zen\Config\AbstractContainerConfig;
+use WoohooLabs\Zen\Config\Hint\DefinitionHint;
+use WoohooLabs\Zen\Config\Hint\DefinitionHintInterface;
+use WoohooLabs\Zen\Exception\ContainerException;
 
 /**
  * Simplified provider definition
@@ -14,10 +18,41 @@ class Provider
 {
 	use ProvidesContainerConfig;
 
-	public function __construct($entryPoints, $definitionHints = [], $wildcardHints = [])
+	public function __construct($entryPoints = [], $definitionHints = [], $wildcardHints = [])
 	{
 		$this->entryPoints     = $entryPoints;
 		$this->definitionHints = $definitionHints;
 		$this->wildcardHints   = $wildcardHints;
+	}
+
+	/**
+	 * @return DefinitionHintInterface[]
+	 */
+	public function createDefinitionHints(): array
+	{
+		$definitionHints = array_map(
+			function ($definitionHint): DefinitionHintInterface {
+				if ($definitionHint instanceof DefinitionHintInterface) {
+					return $definitionHint;
+				}
+
+				if (\is_string($definitionHint)) {
+					return new DefinitionHint($definitionHint);
+				}
+
+				if (\is_array($definitionHint)) {
+					return new FactoryHint(...$definitionHint);
+				}
+
+				throw new ContainerException("A definition hint must be either a string or a DefinitionHint object");
+			},
+			$this->getDefinitionHints()
+		);
+
+		foreach ($this->getWildcardHints() as $wildcardHint) {
+			$definitionHints = array_merge($definitionHints, $wildcardHint->getDefinitionHints());
+		}
+
+		return $definitionHints;
 	}
 }
